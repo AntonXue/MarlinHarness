823d7d02b666ee4a927698e3882ddea1ddacb1ae
Anton Xue <antonxue1@gmail.com>
2017-05-30 10:08:28 -0400

    holy fuck it compiles

--- src/harness.cpp
+++ src/harness.cpp
@@ -8,13 +8,20 @@
 bool Running = true;
 volatile bool wait_for_heatup = false;
 float current_position [XYZE] = { 0.0 };
-
-void print_heaterstates() {}
-void kill(const char*) {}
-
 Stopwatch print_job_timer = Stopwatch();
-
 bool axis_known_position[XYZ] = { false };
+int16_t fanSpeeds[FAN_COUNT] = { 0 };
+uint8_t marlin_debug_flags = DEBUG_NONE;
+float volumetric_multiplier[EXTRUDERS] = ARRAY_BY_EXTRUDERS1(1.0);
+int flow_percentage[EXTRUDERS] = ARRAY_BY_EXTRUDERS1(100);
+
+void print_heaterstates() {
+    printf("printing heater states\n");
+}
+
+void kill(const char* str) {
+    printf("killing: %s\n", str);
+}
 
 void idle() {
     printf("idling\n");
