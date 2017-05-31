ccf8cfd352d75d76b0c1ee133d229d1b73c77802
Anton Xue <antonxue1@gmail.com>
2017-05-31 03:29:31 -0400

    Able to use the built in command queue stuff for now

--- src/harness.h
+++ src/harness.h
@@ -1,6 +1,14 @@
 #ifndef HARNESS_H
 #define HARNESS_H
 
-void calc_move(float cur[], float tgt[]);
+#define MAX_CMD_BUF_SIZE 96
+#define XYZE_DIM 4
+#define XYZE_X 0
+#define XYZE_Y 1
+#define XYZE_Z 2
+#define XYZE_E 3
+
+// void calc_move(float cur[], float tgt[MAX_CMD_BUF_SIZE][XYZE_DIM]);
+void calc_moves(char* cmds[], int num_cmds=MAX_CMD_BUF_SIZE);
 
 #endif
